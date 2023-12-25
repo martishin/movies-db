2200b034cd6f4a520e762ab8cf35ec6a0aadc230
Alex Martishin <asmartishin@gmail.com>
2023-12-25 05:28:29 +0100

    Set cors headers

--- server/cmd/api/middleware.go
+++ server/cmd/api/middleware.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// not safe, this project uses vite proxy
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://movies-db.martishin.com")
 
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
